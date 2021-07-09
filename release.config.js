00e15f15ac7681de19694b32bd17d8a9b1815966
lucas correia <tsirlucas@gmail.com>
2021-07-09 11:30:47 -0300

    chore: setup yarn workspaces

---
+++ release.config.js
@@ -0,0 +1,91 @@
+const mergeRegx = /^Merge pull request #(.+) from (.*)\n\n/;
+const commitRegx = /^(\w*)(?:\((.*)\))?: (.*)$/;
+module.exports = Promise.resolve()
+  .then(() => require('conventional-changelog-angular'))
+  .then((preset) => {
+    const currentPackage = process.cwd().split('/').pop();
+    const isSDK = currentPackage === 'package';
+
+    const plugins = [
+      [
+        '@semantic-release/commit-analyzer',
+        {
+          preset: 'angular',
+          releaseRules: [{ breaking: true, release: 'minor' }],
+          parserOpts: {
+            mergePattern: /^Merge pull request #(.+) from (.*)/,
+            noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+          },
+        },
+      ],
+      [
+        '@semantic-release/release-notes-generator',
+        {
+          preset: 'angular',
+          parserOpts: {
+            mergePattern: mergeRegx,
+            noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+          },
+          writerOpts: {
+            transform: (commit, context) => {
+              if (!mergeRegx.test(commit.message)) {
+                return false;
+              }
+
+              const [header, type, scope, subject] = commit.body.match(commitRegx) || [];
+
+              if (!header) return false;
+
+              return preset.writerOpts.transform(
+                {
+                  ...commit,
+                  header,
+                  type,
+                  scope,
+                  subject,
+                },
+                context,
+              );
+            },
+          },
+        },
+      ],
+      [
+        '@semantic-release/changelog',
+        {
+          changelogTitle: '#Changelog',
+          changelogFile: `${process.cwd()}/CHANGELOG.md`,
+        },
+      ],
+      [
+        '@semantic-release/npm',
+        {
+          npmPublish: false,
+          // npmPublish: isSDK,
+        },
+      ],
+      [
+        '@semantic-release/git',
+        {
+          assets: [
+            `${process.cwd()}/package.json`,
+            `${process.cwd()}/yarn.lock.json`,
+            '`${process.cwd()}/CHANGELOG.md`',
+          ],
+          message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+        },
+      ],
+    ];
+
+    console.log(process.env);
+
+    return {
+      tagFormat: process.env.TAG_FORMAT,
+      branches: [
+        'master',
+        // { name: 'staging', channel: 'rc', prerelease: 'rc' },
+        { name: 'workspaces', channel: 'next', prerelease: 'next' },
+      ],
+      plugins,
+    };
+  });
